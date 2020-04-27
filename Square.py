58c4241be5d950bb90bc26b496a2400b7787893b
Sonny <48072766+SonnyIsMario@users.noreply.github.com>
2020-04-27 14:53:58 -0400

    Update Square.py

--- Square.py
+++ Square.py
@@ -1,9 +1,18 @@
 import turtle
+import sys
+
 
 print("___________")
 print("|         |")
 print("|         |")
 print("|         |")
-print("-----------")
+print("___________")
 
 t = turtle.Turtle
+
+t.forward(200)
+t.up(200)
+t.left(200)
+t.down(200)
+
+sys.exit()
